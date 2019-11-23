4989bca8ffbfb19277263dd6e36dd2c64c2ed2ad
Julius-717 <waynejulius10@gmail.com>
2019-11-23 21:32:40 +0300

    add details to quote-detail.component

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-quote-detail',
-  templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
 })
-export class QuoteDetailComponent implements OnInit {
+export class QuoteDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();//transmits to the delete trigger from parent component on delete function
+
+  quoteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
 
   ngOnInit() {
   }
