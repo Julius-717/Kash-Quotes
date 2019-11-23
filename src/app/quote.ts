4e449d144acbfc4bc00d8b56ff44e22b3c826d81
Julius-717 <waynejulius10@gmail.com>
2019-11-23 23:17:25 +0300

    change header name

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,13 +1,28 @@
 export class Quote {
+  showDescription: boolean;
 
-showDescription= false;
+  constructor( public id:number,
+               public quoteText:string,
+               public author:string,
+               public quoteSubmitter:string, 
+               public quoteUpVote: number=0,
+               public quoteDownVote: number=0,
 
-  constructor(
-    public id: number,
-    public name: string,
-    public description: string,
-    public submitter: string,
-    public completeDate: Date) {
-}
-  }
+               public dateCreated:Date) {
+
+  this.showDescription= false;
+               }
+              }
+// export class Quote {
+
+// showDescription= false;
+
+//   constructor(
+//     public id: number,
+//     public name: string,
+//     public description: string,
+//     public submitter: string,
+//     public completeDate: Date) {
+// }
+  // }
 //contains properties of all inputs
